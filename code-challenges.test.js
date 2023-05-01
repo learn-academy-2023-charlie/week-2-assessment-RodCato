0a1eee539525c558d3211885d00cfcef6fc9ed66
RodCato <code@eliment.org>
2023-04-30 23:55:47 -0500

    Week 2 Assessments complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,37 +12,111 @@
 
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multiplied by 3.
 
-// Pseudo code:
+// Pseudo code: 
+
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
-// Expected output: [72, 81, 90, 99, 108]
+// const numbersArray1 = [6, 7, 8, 9, 10]
+// // Expected output: [18, 21, 24, 27, 30]
+// const numbersArray2 = [24, 27, 30, 33, 36]
+// // Expected output: [72, 81, 90, 99, 108]
 
 // b) Create the function that makes the test pass.
 
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
 // Pseudo code:
+//input: object with key value pair
+//output: value is or is not divisible by three
+//use conditional statement and modulo operator to see if the value in the object is or is not divisible by three
+//result output of whether value is or is not divisible by three
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const object1 = { number: 15 }
-// Expected output: "15 is divisible by three"
-const object2 = { number: 0 }
-// Expected output: "0 is divisible by three"
-const object3 = { number: -7 }
-// Expected output: "-7 is not divisible by three"
+describe ("testFunction", () => {
+    it("takes an object and determine if the values are divisible by three", () => {
+        const object1 = { number: 15 }
+        // Expected output: "15 is divisible by three"
+        const object2 = { number: 0 }
+        // Expected output: "0 is divisible by three"
+        const object3 = { number: -7 }
+        // Expected output: "-7 is not divisible by three"
+        expect(testFunction(object1)).toEqual("15 is divisible by three")
+        expect(testFunction(object2)).toEqual("0 is divisible by three")
+        expect(testFunction(object3)).toEqual("-7 is not divisible by three")
+    })
+})
+
+// FAIL  ./code-challenges.test.js
+// testFunction
+//   ✕ takes an object and determine if the values are divisible by three
+
+// ● testFunction › takes an object and determine if the values are divisible by three
+
+//   ReferenceError: testFunction is not defined
 
 // b) Create the function that makes the test pass.
+
+// const testFunction = (object) => {
+//     if (object.number % 3 === 0){
+//         return `${object.number} is divisible by three`
+//     } else {
+//         return `${object.number} is not divisible by three`
+//     }
+// }    
+
+// PASS  ./code-challenges.test.js
+// testFunction
+//   ✓ takes an object and determine if the values are divisible by three (1 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+// Snapshots:   0 total
+// Time:        0.145 s, estimated 1 s
+// Ran all test suites.
+// ✨  Done in 0.57s.
+
+//Refactor:
+
+const testFunction = (obj) => { 
+    return obj.number % 3 === 0 ? `${obj.number} is divisible by three` : `${obj.number} is not divisible by three`
+}
+
+// console.log(testFunction(object1))
+// console.log(testFunction(object2))
+// console.log(testFunction(object3))
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // Pseudo code:
 
+//input: array of words
+//output: array with all words capitalized
+//iterate through an array using .map and then use .charAt to pick first letter of the word (index 0) then use .toUpperCase to output the first letter capitalized
+//combine that output with .slice(1) to add the first letter capital to return new array with all words capitalized
+
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("capArr", ()=> {
+    it("takes an array and makes all words capital", () =>{
+        const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+        expect(capArr(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+        const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
+        expect (capArr(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango"])
+
+    })
+})
+
+// FAIL  ./code-challenges.test.js
+// testFunction
+//   ✓ takes an object and determine if the values are divisible by three (1 ms)
+// capArr
+//   ✕ takes an array and makes all words capital (1 ms)
+
+// ● capArr › takes an array and makes all words capital
+
+//   ReferenceError: capArr is not defined
 
 const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
@@ -51,11 +125,55 @@ const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 
 // b) Create the function that makes the test pass.
 
+// const capArr = (array) => {
+//     return array.map((value) => {
+//         return value.charAt(0).toUpperCase() + value.slice(1)
+//     })
+// }
+
+// PASS  ./code-challenges.test.js
+// testFunction
+//   ✓ takes an object and determine if the values are divisible by three (1 ms)
+// capArr
+//   ✓ takes an array and makes all words capital (1 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
+// Snapshots:   0 total
+// Time:        0.141 s
+// Ran all test suites.
+
+//Refactor:
+
+const capArr = (array) => { 
+    return array.map((value) => value.charAt(0).toUpperCase() + value.slice(1))
+}
+
+// console.log(capArr(randomNouns1))
+// console.log(capArr(randomNouns2))
+
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // Pseudo code:
 
+//input: a string that logs the index of the first vowel
+//output: a function 
+//iterate through a string and log index of first vowel
+//return index from a string with first vowel using the .includes method to compare with a variable containing vowels.
+
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("firstVowel", () => {
+    it("takes a string and logs index of first vowel", () => {
+        const vowelTester1 = "learn"
+        expect (firstVowel(vowelTester1)).toEqual(1)
+        const vowelTester2 = "academy"
+        expect (firstVowel(vowelTester2)).toEqual(0)
+        const vowelTester3 = "challenges"
+        expect (firstVowel(vowelTester3)).toEqual(2)  
+    })
+
+})
 
 const vowelTester1 = "learn"
 // Expected output: 1
@@ -64,4 +182,61 @@ const vowelTester2 = "academy"
 const vowelTester3 = "challenges"
 // Expected output: 2
 
+// FAIL  ./code-challenges.test.js
+// testFunction
+//   ✓ takes an object and determine if the values are divisible by three (1 ms)
+// capArr
+//   ✓ takes an array and makes all words capital
+// firstVowel
+//   ✕ takes a string and logs index of first vowel (1 ms)
+
+// ● firstVowel › takes a string and logs index of first vowel
+
+//   ReferenceError: firstVowel is not defined
+
 // b) Create the function that makes the test pass.
+
+// const firstVowel = (string) => {
+//     const vowels = ["a", "e", "i", "o", "u"];
+//     for (let i = 0; i < string.length; i++) {
+//         if (vowels.includes(string[i])){
+//             return i
+//         }
+//     }
+// }
+
+// PASS  ./code-challenges.test.js
+// testFunction
+//   ✓ takes an object and determine if the values are divisible by three (1 ms)
+// capArr
+//   ✓ takes an array and makes all words capital
+// firstVowel
+//   ✓ takes a string and logs index of first vowel
+
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
+// Snapshots:   0 total
+// Time:        0.15 s, estimated 1 s
+// Ran all test suites.
+// ✨  Done in 0.66s.
+
+//Refactor: Struggled to refactor was only able to do after extensive googling. 
+
+// const firstVowel = (string) => {
+//     const vowels = ["a", "e", "i", "o", "u"];
+//     return string.map (let i = 0; i < string.length; i++) {
+//         if (vowels.includes(string[i])){
+//             return i
+//         }
+//     }
+// }
+
+
+const firstVowel = (string) => {
+    return string.split('').findIndex((char) => ["a", "e", "i", "o", "u"].includes(char));
+}
+
+
+// console.log(firstVowel(vowelTester1))
+// console.log(firstVowel(vowelTester2))
+// console.log(firstVowel(vowelTester3))
--- coding-concepts.js
+++ coding-concepts.js
@@ -8,10 +8,10 @@
 // --------------------1) What will this log?
 
 const cohort = "Charlie 2023"
-// console.log(cohort.split(""))
+// console.log(cohort.split(" "))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: all letters in the string "Charlie 2023" in a separate string.
+// b) Verify and explain: .split is a built in method that will separate a string by each letter when it has the function of "". " "(quotations with a space in the middle) will return each word split into its own string.
 
 // --------------------2) What will this log?
 
@@ -19,25 +19,27 @@ const greeter = (name) => {
   `Hello, ${name}!`
 }
 // console.log(greeter("LEARN Student"))
+//output: undefined
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: undefined
+// b) Verify and explain: The variable greeter does not have a value defined, meaning that name is equal to nothing and so the greeter variable being called returns undefined.
 
 // --------------------3) What will this log?
 
 const multipliedByTwo = [4, 5, 6, 7, 8].map((number) => number * 2)
-// console.log(multipliedByTwo)
+//console.log(multipliedByTwo)
+//output: [ 8, 10, 12, 14, 16 ]
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: returns all values in the array multiplied by 2
+// b) Verify and explain: .map is a function that can iterate through an array, in this case number is the parameter for the index values of the array and number * 2 is all the values in the array multiplied by 2.
 
 // --------------------4) What will this log?
 
 const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
-// console.log(onlyOdds)
-
-// a) Your answer:
-// b) Verify and explain:
+console.log(onlyOdds)
+//output: [ 11, 13, 15 ]
+// a) Your answer: [11, 13, 15]
+// b) Verify and explain: .filter returns a subset of an array. In this case the subset is a number from the array that has a remainder that does not equal 0 indicating that it is an odd number
 
 // --------------------5) What will this log?
 
@@ -47,22 +49,23 @@ const myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+//console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Javascript
+// b) Verify and explain: myCodingSkills is an object with key value pairs .languages indicates the first key value pair and [0] indicates its an array index of 0 which is the value we get of Javascript. 
 
 // --------------------STRETCH: What will this log?
 
 class Learn {
-  constructor(name) {
+  constructor(name, age) {
     this.student = name
     this.cohort = "Charlie"
     this.year = 2023
+    this.age = age
   }
 }
-const learnStudent = new Learn("George")
-// console.log(learnStudent)
+const learnStudent = new Learn("George", 12)
+//console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Learn { student: 'George', cohort: 'Charlie', year: 2023 }
+// b) Verify and explain: the constructor is expecting one argument of name which is unfixed. new Learn redoes the class with the unfixed value of name set to be equal to "George" and the rest of the values equal to their set values. Basically "George" is a new parameter that replaces the unfixed property of name. 
